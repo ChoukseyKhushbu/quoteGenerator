5083dd972678a18d7f1aeb5a4cef1d75d554d0f6
khushbu <khushbu25chouksey@gmail.com>
2020-03-05 04:21:52 +0530

    modified code

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-$("#ajaxbtn").click(function(){
+
+$('document').ready(function(){
+  let response=[];
   $.get('https://type.fit/api/quotes',function (data, textStatus, jqXHR) {
-
-    const response = JSON.parse(data); //to parse the data in JSON format
-
-    // console.log(response);
-    $("#ajaxbtn").hide();
-    let randomQouteNumber = Math.floor(Math.random()*response.length);
-
-				$('h2').append(response[randomQouteNumber].text);
-        $('p').append("- "+response[randomQouteNumber].author);
-
-
+     response = JSON.parse(data); //to parse the data in JSON format
 });
+$("#ajaxbtn").click(function(){
+  let randomQouteNumber = Math.floor(Math.random()*response.length);
+
+  $('h2').text(response[randomQouteNumber].text);
+  $('p').text("- "+response[randomQouteNumber].author);
+});
+
 });
